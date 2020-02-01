c014872a2556fcd5e7ba734a719784eeccbd0d55
chepkoy <allankiplangat22@outlook.com>
2020-02-01 11:00:47 +0300

    Arithmetic Operations

--- numpy_arrays.py
+++ numpy_arrays.py
@@ -1,103 +1,95 @@
 import numpy as np
 
 """
-RESHAPING AND RESIZING:
-    Reshaping an array doesnt require modify the underlying array data
-    It changes how the data is interpreted y redefining the **strides**
-    e.g 2 X 2 matrix reinterreted as a 1 X 4 array (vector)
+VECTORIZED EXPRESSIONS:
+    Storing numerical data ain arrays enables us to process the data with 
+    **vectorised expressions**, representing batch operations that are applied
+    to all elements in the arrays.
     
-    np.reshape can be used to reconfigure how underlying data is interpreted
+    Efficient use if vectorized expression eliminate need of many explicit for 
+    loops
     
-    The requested new shape of the array should match the number of elements
-    in the original size
+    * Less verbose code
+    * Better maintainability
+    * Higher performing code
     
-    The number of axes(dimension) dont need to be conserved
+    Many of the functions and operations acts on the array on the elementwise
+    basis
     
-NOTE: reshaping an array produces a view of the array
-
-If an independent copy of the array is needed the view has to be copied 
-explicitly using np.copy() 
-
-np.ravel collapses all the dimensions of an array and returns a flattened 1D
--> Returns a view
-
-
-ndarray flatten method performs the same function -> Returning a copy
-
-np.ravel and np.flatten collapses the axes of an array into 1D array
-
-INDUCING NEW AXES TO ARRAY:
-    1. By using np.reshape
-    2. When adding new empty axis
-"""
-
-data = np.array([[1,2],[3,4]])
-
-# Reshaping uing np.reshape() method
-np.reshape(data, (1, 4))
-
-# Using the reshape()
-new_data = data.reshape(4)
-
-# Flattening
-flattening = np.array([[1,2], [3,4]])
-flatened = flattening.flatten()
-
-#checking the shape
-flatened.shape
-
-# Introducing new axes
-new_axes = np.arange(0, 5) # One axis
-
-# Indexing with a tuple more than one element
-column = new_axes[:, np.newaxis]
-
-row = new_axes[np.newaxis, :] 
-
-# Adding new dimensions to an array
-equivalent = np.expand_dims(new_axes, axis=1)
-equivalent2 = np.expand_dims(new_axes, axis=0) 
-
-"""
-    axis argument above specifies the location relative to existing axes where 
-    new axis is to be inserted
+    Binary operations require all arrays in an expression to be of compatible
+    size (Variables in an expression represents either scalars or arrays
+    of the same size and shape)
     
-    MERGING ARRAYS:
-        Its often necessary to merge arrays into biger arrays
+    A binary operation between two arrays is well defined if the arrays can
+    be broadcasted into the same shape and size
+    
+    Incase of an operation between a scalar and an array, Broadcasting refers
+    to the scalar being distributed and the operation applied to each element 
+    in the array
+    
+    For an expression containing arrays of unequal sizes the operations may
+    still be well defined if the smaller of the array can be broadcasted
+    ("Effectively expanded") to a much larger array
+    
+    NumPy BROADCASTIGN RULE: 
+        An array can be broadcasted over another array if their **Axes**
+        on a one-by-one basis either have the same length or if either of 
+        them have length 1.
         
-        1. np.vstack - for vertical stacking of rows into a matrix
-        2. np.hstack - for horizontal stacking of columns into a matrix
-        3. np.concat(axis=0)
+        If the number of axes of the two arrays is not equal the arrays with
+        fewer axes is padded with new axes of length 1 from the left until
+        the numbers of dimensions of the two arrays agree.
         
 """
 
-# Stacking a 1D array vertically
-data = np.arange(5)
-
-stacked =  np.vstack((data, data, data))
-
-vdata = np.arange(5)
-np.hstack((data, data, data)) # 1D doesnt work
-
-# Make the input array a 2D array of shape (1, 5)
-# insert new axis by indexing with
-vdata = vdata[:, np.newaxis]
-np.hstack((vdata, vdata, vdata))
 
 """
-    NOTE: To  insert  append and remove elements from numpy array
-    using the functions np.append, np.insert, np.deelete
-    
-    New arrays must be created and data copied to it 
-    
-    Prealocate arrays with size such that they dont need to be resized
-    
+    ARITHMETIC OPERATIONS:
+        The standard operations with NumPy arrays perform elementwise
+        operations.
+        
+        In operations between scalars and arrays the scalar value is applied 
+        to each element in the array
+        
+        The  dtype of the resulting array for an expression can be promoted 
+        if the computation requires it
+        
+        ValueError:
+            If an arithmetic operation is performed on arrays with incompatible 
+            size or shape a ValueError is raised
 """
 
+# (+, -, x, /) of Equal-sized arrays
+X = np.array([[1, 2], [3, 4]])
+Y = np.array([[5, 6], [7, 8]])
+
+X + Y
+Y - X
+X * Y
+Y / X
+
+# Operations between scalars and arrays
+X * 2
+2 ** X
+
+# Division between an iteger array and an Integer scalar
+Y / 2 
+(Y / 2).dtype
+
+# ValueError: operands could not be broadcast together with shapes (2,2) (4,)
+
+A = np.array([1, 2, 3, 4]).reshape(2, 2)
+B = np.array([1, 2, 3, 4])
+
+A / B
 
 
+C = np.array([[2, 4]])
+C.shape
 
+A / C
 
+CC = np.concatenate([C, C], axis=0)
+CC
 
-
-
+A / CC
